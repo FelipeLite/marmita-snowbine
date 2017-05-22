e18f720467a36856a0ade6ce8355a4b48a95716f
f_nev <f_nev@192.168.50.54>
2017-05-22 17:35:37 -0300

    update 2
    nao mexer no login

--- marmita-snowbine/src/main/java/br/com/snowbine/base/bean/BaseBean.java
+++ marmita-snowbine/src/main/java/br/com/snowbine/base/bean/BaseBean.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import br.com.snowbine.base.dao.GenericDao;
 
 public class BaseBean<T extends Serializable, D extends GenericDao<T>>
@@ -12,48 +14,56 @@ public class BaseBean<T extends Serializable, D extends GenericDao<T>>
 	private D dao;
 	private T entidade;
 	private List<T> listaEntidade;
-	
+
 	private Class<T> classeEntidade;
 	private Class<D> classeDao;
-	
-	@SuppressWarnings("unchecked")
+
 	public BaseBean()
 	{
-		//Encontra a classe
+
+	}
+
+	@PostConstruct
+	public void init()
+	{
+		System.out.println("DENTRO D POS CONTRUCT BEIBE");
+		
+		// Encontra a classe
 		classeEntidade = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
 		classeDao = (Class<D>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[1];
-		
+
 		try
 		{
 			dao = classeDao.newInstance();
 			entidade = classeEntidade.newInstance();
-			
+
 			this.listar();
 		} 
+		
 		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public List<T> listar()
 	{
 		try
 		{
 			Method metodoListar = dao.getClass().getMethod("listarTodos");
-			this.setListaEntidade((List<T>)metodoListar.invoke(dao));
-			
-		} 
-		
+			this.setListaEntidade((List<T>) metodoListar.invoke(dao));
+
+		}
+
 		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
-		
+
 		return this.getListaEntidade();
 	}
-	
+
 	public String cadastrar()
 	{
 		try
@@ -61,49 +71,54 @@ public class BaseBean<T extends Serializable, D extends GenericDao<T>>
 			Method salvar = dao.getClass().getMethod("salvar", Serializable.class);
 			salvar.invoke(dao, entidade);
 		}
-		
+
 		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
-		
+
 		return "";
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public String editar(Integer id, String nomeEntidade)
 	{
 		try
 		{
 			Method consultarPorId = dao.getClass().getMethod("consultarPorId", Integer.class);
-			this.entidade = (T)consultarPorId.invoke(dao, id);
+			this.entidade = (T) consultarPorId.invoke(dao, id);
 		}
-		
+
 		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
-		
+
 		return "form" + nomeEntidade;
 	}
-	
+
 	public String listagem(String nomeEntidade)
 	{
 		return "list" + nomeEntidade;
 	}
-	
+
 	public void setListaEntidade(List<T> lista)
 	{
 		this.listaEntidade = lista;
 	}
-	
+
 	public List<T> getListaEntidade()
 	{
 		return listaEntidade;
 	}
-	
+
 	public T getEntidade()
 	{
 		return entidade;
+	}
+
+	public D getDao()
+	{
+		return dao;
 	}
 }
--- marmita-snowbine/src/main/java/br/com/snowbine/bean/UsuarioBean.java
+++ marmita-snowbine/src/main/java/br/com/snowbine/bean/UsuarioBean.java
@@ -4,12 +4,9 @@ import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.bean.ViewScoped;
 
 import br.com.snowbine.base.bean.BaseBean;
-import br.com.snowbine.dao.EstadoDao;
 import br.com.snowbine.dao.UsuarioDao;
-import br.com.snowbine.entity.Estado;
 import br.com.snowbine.entity.Usuario;
 import br.com.snowbine.util.StringUtils;
 
@@ -26,5 +23,58 @@ public class UsuarioBean extends BaseBean<Usuario,UsuarioDao> implements Seriali
 		this.getEntidade().setSenha(StringUtils.criptografarSenha(this.getEntidade().getSenha()));
 		
 		return super.cadastrar();
+	}
+	
+	public String logar()
+	{
+		System.out.println("[Login e verificação]");
+		if(!this.validarLoginSenha())
+		{
+			return null;
+		}
+		
+		return null;
+	}
+	
+	private boolean validarLoginSenha()
+	{
+		String login = this.getEntidade().getLogin().trim();
+		String senha = this.getEntidade().getSenha().trim();
+		
+		
+		try
+		{
+			//Procura pelo usuario
+			Integer resultado = this.getDao().consultarPorParametros("t.login = '" + login + "'").size();
+			System.out.println(resultado);
+			
+			if(resultado == 0)
+			{
+				System.out.println("Usuario nao encontrado!");
+				return false;
+			}
+			
+			else
+			{
+				//Encontra o usuario e compara a senha
+				Usuario usuarioEncontrado = this.getDao().consultarPorParametros("login =" + login).get(0);
+				
+				if(!StringUtils.criptografarSenha(senha).equals(usuarioEncontrado.getSenha()))
+				{
+					System.out.println("Usuario ou senha incorretos!");
+					
+					return false;
+				}
+			}
+			
+			System.out.println("login e senha ok");
+			return true;
+		} 
+		
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
